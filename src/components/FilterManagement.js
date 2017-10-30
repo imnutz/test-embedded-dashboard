e35984e431bdedffd791e716f274fddb3a1cc28d
Binh Nguyen <binh.nguyen@gooddata.com>
2017-10-30 13:52:12 +0700

    commit04: fix update multiple filters

--- src/components/FilterManagement.js
+++ src/components/FilterManagement.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
-import {Card, CardActions, CardHeader, CardText} from "material-ui/Card";
-import _ from 'lodash';
+import _ from "lodash";
 import Filter from "./Filter";
 import data from "../data";
 
@@ -75,29 +74,40 @@ class FilterManagement extends Component {
         }
     };
 
+    /*
+     [{
+        label:"created.aag81lMifn6q"
+        value:"2010"
+     },
+     {
+         label:"label.stage.name.stagename"
+         value:"Interest"
+     }]
+
+    */
+
+
     updateFilter = data => {
         !_.isEmpty(data) &&
         this.setState(prevState => {
-
-            const label = data[0].label;
             const values = data.map(({value}) => value);
-
             return {
                 filters: prevState.filters.map((filter) => {
-                    return label !== filter.label ?
-                        filter :
-                        {
+                    let inputFilter = data.find(item => item.label === filter.label);
+                    if (inputFilter) {
+                        return {
                             ...filter,
                             items: filter.items.map(item => {
                                 return values.findIndex(value => item.value === value) >= 0  ?
-                                {...item, checked: true} :
-                                {...item, checked: false}
+                                    {...item, checked: true} :
+                                    {...item, checked: false}
                             })
-                        }
+                        };
+                    }
+                    return filter;
                 })
             }
-        })
-
+        });
     };
 
     render () {
