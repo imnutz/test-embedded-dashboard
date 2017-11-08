44cb51f7253c9a8dfde055558050588a0045df71
Binh Nguyen <binh.nguyen@gooddata.com>
2017-11-08 11:35:42 +0700

    commit08: refactor json file

--- src/data/index.js
+++ src/data/index.js
@@ -2,139 +2,75 @@ export default {
     embeddedUrl: "https://client-demo.na.intgdc.com:50010/dashboard.html#project=/gdc/projects/qpxew0i8wklcncpwxn8reu20nbra0e3j&dashboard=/gdc/md/qpxew0i8wklcncpwxn8reu20nbra0e3j/obj/75534&tab=ac0b055dd728",
     filters: `[
         {
-            "title": "Sale Rep",
-            "label": "label.owner.id.name",
-            "items": [
-                {
-                    "value" : "Adam Bradley"
-                },
-                {
-                    "value" : "Alejandro Vabiano"
-                },
-                {
-                    "value" : "Alexsandr Fyodr"
-                },
-                {
-                    "value" : "Cory Owens"
-                },
-                {
-                    "value" : "Lea Forbes"
-                },
-                {
-                    "value" : "Monique Babonas"
-                },
-                {
-                    "value" : "Paul Gomez"
-                },
-                {
-                    "value" : "Paul Jacobs"
-                },
-                {
-                    "value" : "Ravi Deetri"
-                },
-                {
-                    "value" : "Thomas Gones"
-                },
-                {
-                    "value" : "Tom Stickler"
-                },
-                {
-                    "value" : "Trevor Deegan"
-                },
-                {
-                    "value" : "Victor Crushetz"
-                }
+            title: 'Sale Rep',
+            label: 'label.owner.id.name',
+            items: [
+                {value : 'Adam Bradley'},
+                {value : 'Alejandro Vabiano'},
+                {value : 'Alexsandr Fyodr'},
+                {value : 'Cory Owens'},
+                {value : 'Lea Forbes'},
+                {value : 'Monique Babonas'},
+                {value : 'Paul Gomez'},
+                {value : 'Paul Jacobs'},
+                {value : 'Ravi Deetri'},
+                {value : 'Thomas Gones'},
+                {value : 'Tom Stickler'},
+                {value : 'Trevor Deegan'},
+                {value : 'Victor Crushetz'}
             ]
         },
         {
-            "title": "Activity Type",
-            "label": "label.activity.activitytype",
-            "items": [
-                {
-                    "value": "Email"
-                },
-                {
-                    "value": "In Person Meeting"
-                },
-                {
-                    "value": "Phone Call"
-                },
-                {
-                    "value": "Web Meeting"
-                },
-                {
-                    "value": "N/A"
-                }
+            title: 'Activity Type',
+            label: 'label.activity.activitytype',
+            items: [
+                {value : 'Email'},
+                {value : 'In Person Meeting'},
+                {value : 'Phone Call'},
+                {value : 'Web Meeting'},
+                {value : 'N/A'}
             ]
         },
         {
             title: 'Stage Name',
             label: 'label.stage.name.stagename',
             items: [
-                {
-                    value: 'Interest'
-                },
-                {
-                    value: 'Discovery'
-                },
-                {
-                    value: 'Short List'
-                },
-                {
-                    value: 'Risk Assessment'
-                },
-                {
-                    value: 'Conviction'
-                },
-                {
-                    value: 'Closed Won'
-                },
-                {
-                    value: 'N/A'
-                }
+                {value : 'Interest'},
+                {value : 'Discovery'},
+                {value : 'Short List'},
+                {value : 'Risk Assessment'},
+                {value : 'Conviction'},
+                {value : 'Closed Won'},
+                {value : 'N/A'}
             ]
         },
         {
             title: 'Year (Created)',
             label: 'created.aag81lMifn6q',
             items: [
-                {
-                    value: '2007'
-                },
-                {
-                    value: '2008'
-                },
-                {
-                    value: '2009'
-                },
-                {
-                    value: '2010'
-                },
-                {
-                    value: '2011'
-                },
-                {
-                    value: '2012'
-                },
-                {
-                    value: '2013'
-                }
+                {value : '2007'},
+                {value : '2008'},
+                {value : '2009'},
+                {value : '2010'},
+                {value : '2011'},
+                {value : '2012'},
+                {value : '2013'}
             ]
         },
         {
             title: 'Activity',
-            label: "label.activity.id.subject",
+            label: 'label.activity.id.subject',
             items: [
-                {value: "Email with 1000Bulbs.com on Apr-21-08"},
-                {value: "Email with 1000Bulbs.com on Dec-14-09"},
-                {value: "Email with 1000Bulbs.com on Dec-29-08"},
-                {value: "Email with 1000Bulbs.com on Feb-10-09"},
-                {value: "Email with 1000Bulbs.com on Jan-06-10"},
-                {value: "Email with 1000Bulbs.com on Jan-29-09"},
-                {value: "Email with 1000Bulbs.com on Jul-03-08"},
-                {value: "Email with 1000Bulbs.com on Jul-06-10"},
-                {value: "Email with 1000Bulbs.com on Jul-24-10"},
-                {value: "Email with 1000Bulbs.com on Jun-01-10"}
+                {value : 'Email with 1000Bulbs.com on Apr-21-08'},
+                {value : 'Email with 1000Bulbs.com on Dec-14-09'},
+                {value : 'Email with 1000Bulbs.com on Dec-29-08'},
+                {value : 'Email with 1000Bulbs.com on Feb-10-09'},
+                {value : 'Email with 1000Bulbs.com on Jan-06-10'},
+                {value : 'Email with 1000Bulbs.com on Jan-29-09'},
+                {value : 'Email with 1000Bulbs.com on Jul-03-08'},
+                {value : 'Email with 1000Bulbs.com on Jul-06-10'},
+                {value : 'Email with 1000Bulbs.com on Jul-24-10'},
+                {value : 'Email with 1000Bulbs.com on Jun-01-10'}
             ]
         }
     ]`
